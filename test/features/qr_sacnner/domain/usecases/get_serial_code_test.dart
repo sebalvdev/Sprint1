f5302340ded7f9e2765335378aa53b5a1899a9af
Sebastian Alvarez <sebastianalvarezb1@gmail.com>
2024-02-23 10:33:33 -0400

    complete test presentation QR Sscanner

--- test/features/qr_sacnner/domain/usecases/get_serial_code_test.dart
+++ test/features/qr_sacnner/domain/usecases/get_serial_code_test.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:sprint1/core/error/failures.dart';
 import 'package:sprint1/features/qr_scanner/domain/repositories/qr_scanner_repository.dart';
 import 'package:sprint1/features/qr_scanner/domain/usecases/review_serial_code.dart';
 import 'package:mocktail/mocktail.dart';
@@ -27,4 +28,20 @@ final result = await getQcCode(const Params(serial: tSerial));
   verify(() => qrScannerRepository.reviewSerial(tSerial));
   verifyNoMoreInteractions(qrScannerRepository);
  });
+
+ test(
+    "should return failure when the repository fails",
+    () async {
+      // arrange
+      final Failure failure = UnkownFailure();
+      when(() => qrScannerRepository.reviewSerial(tSerial))
+          .thenAnswer((_) async => Left(failure));
+      // act
+      final result = await getQcCode(const Params(serial: tSerial));
+      // assert
+      expect(result, Left(failure));
+      verify(() => qrScannerRepository.reviewSerial(tSerial));
+      verifyNoMoreInteractions(qrScannerRepository);
+    },
+  );
 }
